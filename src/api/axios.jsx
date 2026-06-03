ac33b2ca046eec147375dc1888a04b2f070e8eec
Sonam <sonamver274@gmail.com>
2026-06-04 01:16:04 +0530

    fix cors - remove withCredentials, add interceptor

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,9 +1,14 @@
 import axios from "axios";
 
-const api = axios.create(
-{
+const api = axios.create({
     baseURL:"https://job-board-backend-zwyb.onrender.com/api/v1",
-    withCredentials: true,
-}
-)
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 export default api 
